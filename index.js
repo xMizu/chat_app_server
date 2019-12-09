2cf3e7d7435a90b7f35556ded6c052267198e2ac
Anthony Wan <Anthonywan00@gmail.com>
2019-12-08 21:42:10 -0500

    created socket connection to client

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3100;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.get("/", (req, res) => {
   res.send("Server is up an running");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+io.on("connection", socket => {
+  console.log("user joined");
+
+  socket.join("chatroom");
+
+  socket.on("disconnect", () => {
+    console.log(`user has left, ${socket.id}`);
+  });
+
+  socket.on("sendMsg", (data, cb) => {
+    console.log(data);
+    io.to("chatroom").emit("newMsg", data);
+    cb();
+  });
 });
